fca47a85dfd2a1e6583f7ae103a4bf06a8889e52
KARTHIKEYANREDDY MANDADI <109977197+karthikeyan-reddy@users.noreply.github.com>
2023-05-19 09:46:38 +0530

    Add files via upload

--- DbFirstApproach.cs
+++ DbFirstApproach.cs
@@ -1,1 +1,24 @@
-﻿
+﻿using SampleMVCApp.Data;
+namespace SampleMVCApp.Models
+{
+    public interface IDBFirstComponent
+    {
+        void AddNewEmployee(TblEmployee emp);
+        List<TblEmployee> GetAllEmployees();
+    }
+    public class DBFirstComponent : IDBFirstComponent
+    {
+        public void AddNewEmployee(TblEmployee emp)
+        {
+            var context = new KarthikContext();
+            context.TblEmployees.Add(emp);
+            context.SaveChanges();
+        }
+        public List<TblEmployee> GetAllEmployees()
+        {
+            var context = new KarthikContext();
+            var data = context.TblEmployees.ToList();
+            return data;
+        }
+    }
+}
---
+++ DbFirstController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SampleMVCApp.Data;
+using SampleMVCApp.Models;
+
+namespace SampleMVCApp.Controllers
+{
+    public class DbFirstController : Controller
+    {
+        private readonly IDBFirstComponent component;
+        public DbFirstController(IDBFirstComponent component)
+        {
+            this.component = component;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult AllEmployees()
+        {
+            var model = component.GetAllEmployees();
+            return PartialView(model);
+        }
+        public IActionResult AddNew()
+        {
+            var model = new TblEmployee();
+            return PartialView(model);
+        }
+        [HttpPost]
+        public IActionResult AddNew(TblEmployee postedData) 
+        {
+            component.AddNewEmployee(postedData);
+            return RedirectToAction("Index");
+        }
+    }
+}
